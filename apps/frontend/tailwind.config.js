e70a2c50bebf1e235b37d9265d7b9b713d55d6b5
Harris <htom380@aucklanduni.ac.nz>
2022-05-28 03:03:24 +1200

    ✏️ Modify TW config

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,7 +1,15 @@
+const { join } = require('path');
+
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+
 module.exports = {
-  content: [],
+  presets: [require('../../tailwind.config.js')],
+  content: [
+    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
